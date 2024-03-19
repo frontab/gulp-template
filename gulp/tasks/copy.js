f887d8b3371498abbfa4c7b2e27a93851efa1ee4
Andrey Bondarenkov <me@bondarenkov.ru>
2024-03-19 19:55:18 +0500

    fix task: copy

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,13 +1,11 @@
 import {
   src, dest, watch, series, lastRun,
 } from 'gulp';
-import plumber from 'gulp-plumber';
 import { serverReload } from './server';
 import config from '../config';
 
 const copyBuild = () => (
   src(config.copy.src, { base: config.copy.base, since: lastRun(copyBuild) })
-    .pipe(plumber())
     .pipe(dest(config.copy.dest))
 );
 
